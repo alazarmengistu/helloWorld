412f54a2ac7f3992fd38a28f0a4078b8ceeada00
alazarmengistu <87671815+alazarmengistu@users.noreply.github.com>
2021-08-12 14:15:12 -0700

    added a script to the html page and edited the javascript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// when you see '=' it indicates an assignment
+var firstName = "Alazar";
+let lastName = "Mengistu";
+// You cannot reassign constant variables!
+const age = 17;
+// variable reassignments
+firstName = "My Dude";
+lastName = "Potates";
+
+console.log('Hello ' + firstName + ' ' + lastName + '!' + ' You are ' + age + ' years old!');
